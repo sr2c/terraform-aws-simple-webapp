da2a5cc6a7faf30703a23b56866f3a08766044d6
Iain Learmonth <irl@sr2.uk>
2023-07-30 17:00:59 +0100

    outputs: adds instance id

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
+output "instance_id" {
+  description = "ID of the EC2 instance created."
+  value       = module.instance.id
+}
+
 output "postgres_database_uri" {
   description = "URI to access the database created in the RDS instance."
-  value = local.postgres_database_uri
+  value       = local.postgres_database_uri
 }
