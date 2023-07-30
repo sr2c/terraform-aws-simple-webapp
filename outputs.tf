c25198a4b10a8c8d3c884e68705ecda1b068a829
Iain Learmonth <irl@sr2.uk>
2023-07-30 20:51:43 +0100

    outputs: add ec2 instance public ip

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "configuration_bucket" {
   description = "Name of S3 bucket to be used by Ansible SSM connection plugin."
-  value = module.conf_log.conf_bucket_id
+  value       = module.conf_log.conf_bucket_id
 }
 
 output "instance_id" {
@@ -8,9 +8,14 @@ output "instance_id" {
   value       = module.instance.id
 }
 
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance."
+  value       = module.instance.public_ip
+}
+
 output "logging_bucket" {
   description = "Name of S3 bucket which may be used to hold log outputs."
-  value = module.conf_log.log_bucket_id
+  value       = module.conf_log.log_bucket_id
 }
 
 output "postgres_database_uri" {
